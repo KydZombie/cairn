c6f6bb2d9a26961eff1f89480b2e144c94b4810f
Logan Reynolds <gizmordo@gmail.com>
2024-11-26 21:54:24 -0500

    Removed server-sided BlockEntity mixin

--- src/main/java/io/github/kydzombie/cairn/impl/mixin/BlockEntityMixin.java
+++ src/main/java/io/github/kydzombie/cairn/impl/mixin/BlockEntityMixin.java
@@ -1,19 +1,33 @@
 package io.github.kydzombie.cairn.impl.mixin;
 
+import io.github.kydzombie.cairn.api.block.entity.UpdatePacketReceiver;
+import io.github.kydzombie.cairn.api.packet.BlockEntityUpdatePacket;
 import io.github.kydzombie.cairn.api.storage.AutoNbt;
 import io.github.kydzombie.cairn.api.storage.HasItemStorage;
 import io.github.kydzombie.cairn.api.storage.ItemStorage;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.packet.Packet;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.lang.reflect.Field;
 
 @Mixin(BlockEntity.class)
 public class BlockEntityMixin {
+    @Shadow
+    public int x;
+    @Shadow
+    public int y;
+    @Shadow
+    public int z;
+
     @Inject(method = "readNbt", at = @At("RETURN"))
     private void cairn_injectReadNbt(NbtCompound nbt, CallbackInfo ci) {
         if (!(this instanceof HasItemStorage)) return;
@@ -47,6 +61,14 @@ public class BlockEntityMixin {
                     throw new RuntimeException(e);
                 }
             }
+        }
+    }
+
+    @Environment(EnvType.SERVER)
+    @Inject(method = "createUpdatePacket", at = @At("HEAD"), cancellable = true)
+    private void cairn_customUpdatePacket(CallbackInfoReturnable<Packet> cir) {
+        if (this instanceof UpdatePacketReceiver<?> receiver) {
+            cir.setReturnValue(new BlockEntityUpdatePacket<>(x, y, z, receiver.createUpdateData()));
         }
     }
 }
